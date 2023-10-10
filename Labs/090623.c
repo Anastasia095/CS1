1f1aa1c3723109ef2d56ce0f022a9f96a4879214
Ana Ubuntu <anastas.tarasenko001@mymdc.net>
2023-10-10 19:48:20 -0400

    add lab 1 example

---
+++ Labs/090623.c
@@ -0,0 +1,65 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct author_t{
+    char name[20];
+    int birthYear;
+    int publishings;
+} author_t;
+
+typedef struct book_t{
+    char name[20];
+    int year;
+    int numAuthors;
+    author_t * authors;
+} book_t;
+
+int main(){
+
+    book_t ** prtBookArr; //advanced
+
+    /*Part 1 BEGIN*/
+
+    int i, j, numBooks;
+    book_t * bookArr; //simple
+
+    //Read in the number of books
+    scanf("%d", &numBooks);
+
+    //Allocate memory for the books
+    bookArr = (book_t *) calloc(numBooks, sizeof(book_t));
+    for (i = 0; i < numBooks; i++){
+        scanf("%s", bookArr[i].name);
+        scanf("%d", &bookArr[i].year);
+        scanf("%d", &bookArr[i].numAuthors);
+        bookArr[i].authors = (author_t *) calloc(bookArr[i].numAuthors, sizeof(author_t));
+        for (j = 0; j < bookArr[i].numAuthors; j++){
+            scanf("%s", bookArr[i].authors[j].name);
+            scanf("%d", &bookArr[i].authors[j].birthYear);
+            scanf("%d", &bookArr[i].authors[j].publishings);
+        }
+    }
+
+    /* part 1 END*/
+
+    for (i = 0; i <numBooks; i++){
+        printf("%s\n", bookArr[i].name);
+        for(j = 0; j < bookArr[i].numAuthors; j++){
+            printf("%s\n", bookArr[i].authors[j].name);
+        }
+        printf("\n");
+    }
+
+    /* part 2 BEGIN */
+
+    for( i = 0; i < numBooks; i++){
+        free(bookArr[i].authors);
+        bookArr[i].authors = NULL;
+    }
+    free(bookArr);
+    bookArr = NULL;
+    /*part 2 END*/
+
+    //return a seccess
+    return EXIT_SUCCESS;
+}
